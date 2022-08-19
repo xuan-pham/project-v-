eb1d05e5a0f5a00c01d968fbfe3beb7a97bf812e
xuanpham <pham.xuan.duc.23.09.2019@gmail.com>
2022-08-19 11:04:19 +0700

    add swagger

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('project-v1')
+    .setDescription('the project-v1 API description')
+    .setVersion('1.0')
+    .addTag('project-v1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
+}
+
+bootstrap().then();
